0839cc0ae2f3802908b6916e3a135e77131890af
jhair2022 <luigijhair123@gmail.com>
2025-05-26 14:05:42 -0500

    a

---
+++ assets/js/camara-wifi.js
@@ -0,0 +1,17 @@
+function mostrarBreadcrumb(subcategoria = null) {
+    const breadcrumb = document.getElementById('breadcrumb-producto');
+    breadcrumb.innerHTML = `
+        <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
+        <li class="breadcrumb-item"><a href="camara-wifi.html">Cámaras</a></li>
+        ${subcategoria ? `<li class="breadcrumb-item active" aria-current="page">${capitalizar(subcategoria)}</li>` : ''}
+    `;
+}
+
+function capitalizar(texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
+
+function verProducto(id) {
+    localStorage.setItem("productoSeleccionado", id);
+    window.location.href = "producto.html";
+}
