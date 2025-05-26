4b92abb5e1ee6b6cefba7788ad95056f1d46e4fa
jhair2022 <luigijhair123@gmail.com>
2025-05-26 14:17:32 -0500

    Update camara-wifi.js

--- assets/js/camara-wifi.js
+++ assets/js/camara-wifi.js
@@ -2,7 +2,7 @@ function mostrarBreadcrumb(subcategoria = null) {
     const breadcrumb = document.getElementById('breadcrumb-producto');
     breadcrumb.innerHTML = `
         <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
-        <li class="breadcrumb-item"><a href="camara-wifi.html">Cámaras</a></li>
+        <li class="breadcrumb-item"><a href="camara-wifi.html">Cámaras Wifi</a></li>
         ${subcategoria ? `<li class="breadcrumb-item active" aria-current="page">${capitalizar(subcategoria)}</li>` : ''}
     `;
 }
